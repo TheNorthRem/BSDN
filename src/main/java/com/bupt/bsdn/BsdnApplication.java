fecccb8891ec821bd3ff04737a9b0242687c7770
=TheNorthRem <=1371002466@qq.com>
2023-07-30 20:52:40 +0800

    BSDN

--- src/main/java/com/bupt/bsdn/BsdnApplication.java
+++ src/main/java/com/bupt/bsdn/BsdnApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BsdnApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BsdnApplication.class, args);
     }
