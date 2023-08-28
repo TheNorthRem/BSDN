d673911479d1031ebac9b39cef3dd91e53e26876
Jungle <1239946358@qq.com>
2023-08-28 14:33:48 +0800

    test

--- src/main/java/com/bupt/bsdn/BsdnApplication.java
+++ src/main/java/com/bupt/bsdn/BsdnApplication.java
@@ -1,11 +1,13 @@
 package com.bupt.bsdn;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.bupt.bsdn.mapper")
+@Slf4j
 public class BsdnApplication {
     public static void main(String[] args) {
         SpringApplication.run(BsdnApplication.class, args);
