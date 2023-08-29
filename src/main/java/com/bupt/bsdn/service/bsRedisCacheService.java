dd62b9d3c5ba04ea06eb4985b37612667a955765
Jungle <1239946358@qq.com>
2023-08-29 17:45:59 +0800

    更新了redis配置

---
+++ src/main/java/com/bupt/bsdn/service/bsRedisCacheService.java
@@ -0,0 +1,2 @@
+package com.bupt.bsdn.service;public interface bsRedisCacheService {
+}
---
+++ src/main/java/com/bupt/bsdn/service/impl/bsRedisCacheServiceImpl.java
@@ -0,0 +1,7 @@
+package com.bupt.bsdn.service.impl;/** 
+* @Description: TODO
+* @author: scott
+* @date: 2023年08月29日 17:25
+*/
+public class bsRedisCacheServiceImpl {
+}
