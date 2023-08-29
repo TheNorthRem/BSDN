0287a1cd51c961944a07e79ac1137c626d99f911
Jungle <1239946358@qq.com>
2023-08-29 22:13:52 +0800

    解决跨域请求问题

--- src/main/java/com/bupt/bsdn/controller/bsCommentsController.java
+++ src/main/java/com/bupt/bsdn/controller/bsCommentsController.java
@@ -4,14 +4,12 @@ package com.bupt.bsdn.controller;
 import com.alibaba.fastjson2.JSONObject;
 import com.bupt.bsdn.config.Result;
 import com.bupt.bsdn.entity.bsComments;
+import com.bupt.bsdn.service.bsCommentsService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import com.bupt.bsdn.service.bsCommentsService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/bsComments")
--- src/main/java/com/bupt/bsdn/controller/bsMessageController.java
+++ src/main/java/com/bupt/bsdn/controller/bsMessageController.java
@@ -3,14 +3,12 @@ package com.bupt.bsdn.controller;
 import com.alibaba.fastjson2.JSONObject;
 import com.bupt.bsdn.config.Result;
 import com.bupt.bsdn.entity.bsMessage;
+import com.bupt.bsdn.service.bsMessageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import com.bupt.bsdn.service.bsMessageService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/bsMessage")
