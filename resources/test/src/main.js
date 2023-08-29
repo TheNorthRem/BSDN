e0344c229f93b986d1c0c53de1f17a8ea8065af1
Amy <dingxiaoyu168@outlook.com>
2023-08-29 13:56:44 +0800

    前端页面

---
+++ resources/test/src/main.js
@@ -0,0 +1,116 @@
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+
+import Person from "./pages/Person.vue";
+import Register from "./pages/Register.vue";
+import Login from "./pages/Login.vue";
+import Ccategorization from "./pages/Ccategorization.vue";
+import Article from "./pages/Article.vue";
+import Home from "./pages/Home.vue";
+import Frame from "./pages/Frame.vue";
+import Frame1 from "./pages/Frame1.vue";
+import Frame2 from "./pages/Frame2.vue";
+import Frame3 from "./pages/Frame3.vue";
+import Frame4 from "./pages/Frame4.vue";
+import Header from "./pages/Header.vue";
+import ExportToCodeErrorsDelete from "./pages/ExportToCodeErrorsDelete.vue";
+import "./global.css";
+
+const routes = [
+  {
+    path: "/",
+    name: "Person",
+    component: Person,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/ccategorization",
+    name: "Ccategorization",
+    component: Ccategorization,
+  },
+  {
+    path: "/article",
+    name: "Article",
+    component: Article,
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/",
+    name: "Frame",
+    component: Frame,
+  },
+  {
+    path: "/1",
+    name: "Frame1",
+    component: Frame1,
+  },
+  {
+    path: "/2",
+    name: "Frame2",
+    component: Frame2,
+  },
+  {
+    path: "/3",
+    name: "Frame3",
+    component: Frame3,
+  },
+  {
+    path: "/4",
+    name: "Frame4",
+    component: Frame4,
+  },
+  {
+    path: "/header",
+    name: "Header",
+    component: Header,
+  },
+  {
+    path: "/export-to-code-errors-delete-me-anytime",
+    name: "ExportToCodeErrorsDelete",
+    component: ExportToCodeErrorsDelete,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((toRoute, fromRoute, next) => {
+  const documentTitle =
+    toRoute?.meta && toRoute?.meta?.title ? toRoute?.meta?.title : "test";
+  window.document.title = documentTitle;
+  if (toRoute?.meta?.description) {
+    addMetaTag(toRoute?.meta?.description);
+  }
+  next();
+});
+
+const addMetaTag = (value) => {
+  let element = document.querySelector(`meta[name='description']`);
+
+  if (element) {
+    element.setAttribute("content", value);
+  } else {
+    element = `<meta name="description" content="${value}" />`;
+    document.head.insertAdjacentHTML("beforeend", element);
+  }
+};
+
+createApp(App).use(router).mount("#app");
+
+export default router;
---
+++ resources/test/src/pages/ExportToCodeErrorsDelete.vue
@@ -0,0 +1,69 @@
+<template>
+  <div :class="$style.exportToCodeErrorsDelete">
+    <div :class="$style.needsToUseAutolayout" />
+    <div :class="$style.headerNeedsToUseAutolayou" />
+    <div :class="$style.divblockbasicWrapperChaxw" />
+    <div :class="$style.divblockbasicWrapperChaxw1" />
+    <div :class="$style.divblockbasicWrapperChaxw2" />
+  </div>
+</template>
+<script>
+  import { defineComponent, ref } from "vue";
+
+  export default defineComponent({
+    name: "ExportToCodeErrorsDelete",
+  });
+</script>
+<style module>
+  .needsToUseAutolayout {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    border: 4px solid var(--color-red);
+    box-sizing: border-box;
+    width: 500px;
+    height: 523px;
+  }
+  .headerNeedsToUseAutolayou {
+    position: absolute;
+    top: 13px;
+    left: 0px;
+    border: 4px solid var(--color-red);
+    box-sizing: border-box;
+    width: 496px;
+    height: 94px;
+  }
+  .divblockbasicWrapperChaxw {
+    position: absolute;
+    top: 228px;
+    left: 47px;
+    border: 4px solid var(--color-red);
+    box-sizing: border-box;
+    width: 409px;
+    height: 64px;
+  }
+  .divblockbasicWrapperChaxw1 {
+    position: absolute;
+    top: 127px;
+    left: 47px;
+    border: 4px solid var(--color-red);
+    box-sizing: border-box;
+    width: 409px;
+    height: 64px;
+  }
+  .divblockbasicWrapperChaxw2 {
+    position: absolute;
+    top: 413px;
+    left: 46px;
+    border: 4px solid var(--color-red);
+    box-sizing: border-box;
+    width: 409px;
+    height: 64px;
+  }
+  .exportToCodeErrorsDelete {
+    position: relative;
+    width: 500px;
+    height: 523px;
+    overflow: hidden;
+  }
+</style>
