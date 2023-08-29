f26c9d5d1e0489807705ed9839098da45a7ce0d2
Jungle <1239946358@qq.com>
2023-08-29 18:43:30 +0800

    Redis服务层接口(未测试)

--- src/main/java/com/bupt/bsdn/service/bsRedisCacheService.java
+++ src/main/java/com/bupt/bsdn/service/bsRedisCacheService.java
@@ -1,2 +1,29 @@
-package com.bupt.bsdn.service;public interface bsRedisCacheService {
+package com.bupt.bsdn.service;
+
+/**
+ * @Description: Redis缓存层，用来存储token令牌
+ * @author: syc
+ * @date: 2023年08月29日 17:25
+ */
+public interface bsRedisCacheService {
+    /**
+     * 判断是否存在key
+     */
+    Boolean hasToken(String userId);
+
+    /**
+     * 删除token(登出时使用)
+     */
+    Boolean deleteToken(String key);
+
+    /**
+     * 存储userID-token
+     */
+    void setToken(String userId, String token);
+
+    /**
+     * 根据userId获取token
+     * 若没有或者已经失效则返回null
+     */
+    String getToken(String userId);
 }
--- src/main/java/com/bupt/bsdn/service/impl/bsRedisCacheServiceImpl.java
+++ src/main/java/com/bupt/bsdn/service/impl/bsRedisCacheServiceImpl.java
@@ -1,7 +1,44 @@
-package com.bupt.bsdn.service.impl;/** 
-* @Description: TODO
-* @author: scott
-* @date: 2023年08月29日 17:25
-*/
-public class bsRedisCacheServiceImpl {
+package com.bupt.bsdn.service.impl;
+
+import com.bupt.bsdn.service.bsRedisCacheService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description: Redis缓存层，用来存储token令牌
+ * @author: syc
+ * @date: 2023年08月29日 17:25
+ */
+public class bsRedisCacheServiceImpl implements bsRedisCacheService {
+    private final RedisTemplate<String, String> redisTemplate;
+    private final int expirationTime; //过期时间(秒)
+
+    @Autowired
+    public bsRedisCacheServiceImpl(RedisTemplate<String, String> redisTemplate, @Value("${expirationTime}") int expirationTime) {
+        this.redisTemplate = redisTemplate;
+        this.expirationTime = expirationTime;
+    }
+
+    @Override
+    public Boolean hasToken(String userId) {
+        return redisTemplate.hasKey(userId);
+    }
+
+    @Override
+    public Boolean deleteToken(String key) {
+        return redisTemplate.delete(key);
+    }
+
+    @Override
+    public void setToken(String userId, String token) {
+        redisTemplate.opsForValue().set(userId, token, expirationTime, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public String getToken(String userId) {
+        return redisTemplate.opsForValue().get(userId);
+    }
 }
