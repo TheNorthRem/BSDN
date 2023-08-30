3bbbf8df34100a22f39398bed00c9e8ec641eb9c
Amy <dingxiaoyu168@outlook.com>
2023-08-30 08:52:52 +0800

    整理前端页面

--- resources/test/src/main.js
+++ resources/test/src/main.js
@@ -4,14 +4,9 @@ import App from "./App.vue";
 import Login from "./pages/Login.vue";
 import Person from "./pages/Person.vue";
 import Register from "./pages/Register.vue";
-import Ccategorization from "./pages/Ccategorization.vue";
+import Ccategorization from "./pages/Categorization.vue";
 import Article from "./pages/Article.vue";
 import Home from "./pages/Home.vue";
-import Frame from "./pages/Frame.vue";
-import Frame1 from "./pages/Frame1.vue";
-import Frame2 from "./pages/Frame2.vue";
-import Frame3 from "./pages/Frame3.vue";
-import Frame4 from "./pages/Frame4.vue";
 import Header from "./pages/Header.vue";
 import ExportToCodeErrorsDelete from "./pages/ExportToCodeErrorsDelete.vue";
 import "./global.css";
@@ -47,32 +42,6 @@ const routes = [
     path: "/article",
     name: "Article",
     component: Article,
-  },
-  
-  {
-    path: "/",
-    name: "Frame",
-    component: Frame,
-  },
-  {
-    path: "/1",
-    name: "Frame1",
-    component: Frame1,
-  },
-  {
-    path: "/2",
-    name: "Frame2",
-    component: Frame2,
-  },
-  {
-    path: "/3",
-    name: "Frame3",
-    component: Frame3,
-  },
-  {
-    path: "/4",
-    name: "Frame4",
-    component: Frame4,
   },
   {
     path: "/header",
