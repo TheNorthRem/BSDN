e422183eaa77e906b183e8e00ad4b405fe887b25
Jungle <1239946358@qq.com>
2023-09-06 16:08:41 +0800

    收藏服务层

---
+++ src/main/java/com/bupt/bsdn/service/impl/bsUserFavoritesServiceImpl.java
@@ -0,0 +1,17 @@
+package com.bupt.bsdn.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.bupt.bsdn.mapper.bsUserFavoritesMapper;
+import com.bupt.bsdn.service.bsUserFavoritesService;
+import com.bupt.bsdn.entity.bsUserFavorites;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class bsUserFavoritesServiceImpl extends ServiceImpl<bsUserFavoritesMapper, bsUserFavorites> implements bsUserFavoritesService {
+    private final bsUserFavoritesMapper bsUserFavoritesMapper;
+
+    @Autowired
+    public bsUserFavoritesServiceImpl(bsUserFavoritesMapper bsUserFavoritesMapper) {
+        this.bsUserFavoritesMapper = bsUserFavoritesMapper;
+    }
+}
