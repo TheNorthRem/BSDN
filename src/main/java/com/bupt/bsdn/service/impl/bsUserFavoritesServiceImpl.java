2ac4fc5706dc62d7c64542fb6eccab0b21993f6c
Jungle <1239946358@qq.com>
2023-09-06 16:43:57 +0800

    增加服务层注解

--- src/main/java/com/bupt/bsdn/service/impl/bsUserFavoritesServiceImpl.java
+++ src/main/java/com/bupt/bsdn/service/impl/bsUserFavoritesServiceImpl.java
@@ -5,8 +5,9 @@ import com.bupt.bsdn.mapper.bsUserFavoritesMapper;
 import com.bupt.bsdn.service.bsUserFavoritesService;
 import com.bupt.bsdn.entity.bsUserFavorites;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class bsUserFavoritesServiceImpl extends ServiceImpl<bsUserFavoritesMapper, bsUserFavorites> implements bsUserFavoritesService {
     private final bsUserFavoritesMapper bsUserFavoritesMapper;
 
