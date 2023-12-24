4f49403f8aa5acf5e53138751fd10b4d3b3442ef
Jungle <1239946358@qq.com>
2023-12-24 20:02:59 +0800

    list -> String

--- src/main/java/com/bupt/bsdn/service/bsUserRecommendResultsService.java
+++ src/main/java/com/bupt/bsdn/service/bsUserRecommendResultsService.java
@@ -3,8 +3,6 @@ package com.bupt.bsdn.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.bupt.bsdn.entity.bsUserRecommendResults;
 
-import java.util.List;
-
 public interface bsUserRecommendResultsService extends IService<bsUserRecommendResults> {
-    List<bsUserRecommendResults> recommendList(Integer userId);
+    String recommendList(Integer userId);
 }
